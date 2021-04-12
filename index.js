3a1a62ae6a5e1aeebe9dacbd347ef5109d1f9009
Akhand Patel <akhand3108patel@gmail.com>
2021-04-12 15:37:03 +0530

    Feat Added translation and clear

---
+++ index.js
@@ -0,0 +1,31 @@
+const inputBox = document.querySelector("#text-input")
+const outputBox = document.querySelector("#text-output")
+const translateButton = document.querySelector("#button-translate")
+const clearButton = document.querySelector("#button-clear")
+
+translateButton.addEventListener("click", translateHandler)
+clearButton.addEventListener("click", clearHandler)
+
+function translateHandler(event) {
+  let input = inputBox.value
+  let toFetchUrl =
+    "https://api.funtranslations.com/translate/minion.json?text=" +
+    encodeURI(input)
+
+  console.log(toFetchUrl)
+
+  fetch(toFetchUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data", data)
+      outputBox.innerText = data.contents.translated
+    })
+    .catch((error) => {
+      alert("Some Error ocurred"), console.log(error)
+    })
+}
+
+function clearHandler(event) {
+  inputBox.value = ""
+  outputBox.innerText = ""
+}
